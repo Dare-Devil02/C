515389d9cb4c52f7d85f50488dadf6e0bc4facb5
Abhay <ap.devil48@gmail.com>
2024-11-07 20:59:05 +0530

    Devil

--- Circular.c
+++ Circular.c
@@ -5,29 +5,6 @@ struct Node
     int data;
     struct Node* next;
 };
-void clltraverse(struct Node* head);
-struct Node* insertatfirst(struct Node* head, int value);
-int main()
-{
-    struct Node* head=(struct Node*)malloc(sizeof(struct Node));
-    struct Node* second=(struct Node*)malloc(sizeof(struct Node));
-    struct Node* third=(struct Node*)malloc(sizeof(struct Node));
-    struct Node* fourth=(struct Node*)malloc(sizeof(struct Node));
-
-    head->data=4;
-    head->next=second;
-    second->data=11;
-    second->next=third;
-    third->data=7;
-    third->next=fourth;
-    fourth->data=3;
-    fourth->next=head;
-    printf("List before insertion\n");
-    clltraverse(head);
-    insertatfirst(head,18);
-    printf("\nList after insertion\n");
-    clltraverse(head);
-}
 void clltraverse(struct Node* head)
 {
     struct Node* ptr=head;
@@ -52,4 +29,25 @@ struct Node* insertatfirst(struct Node* head, int data)
     ptr->next=head;
     head=ptr;
     return head;
+}
+int main()
+{
+    struct Node* head=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* second=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* third=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* fourth=(struct Node*)malloc(sizeof(struct Node));
+
+    head->data=4;
+    head->next=second;
+    second->data=11;
+    second->next=third;
+    third->data=7;
+    third->next=fourth;
+    fourth->data=3;
+    fourth->next=head;
+    printf("List before insertion\n");
+    clltraverse(head);
+    head=insertatfirst(head,18);
+    printf("\nList after insertion\n");
+    clltraverse(head);
 }
